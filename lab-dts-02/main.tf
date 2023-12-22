dc8778612d9cdeaca4ca6c5ee8551f497cde40c2
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-22 22:42:28 +0530

    bash script modified

--- lab-dts-02/main.tf
+++ lab-dts-02/main.tf
@@ -31,10 +31,6 @@ resource "aws_instance" "terraformlab02" {
   vpc_security_group_ids = [aws_security_group.tfsglab02.id]
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
               echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
               EOF
   
