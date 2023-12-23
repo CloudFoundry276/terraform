2fab55c229539568e26d3ab99cf6b42d8ced13f1
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-23 20:15:38 +0530

    removed all labs

--- lab-ss-02/app2-install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo service httpd start
-sudo echo '<h1>Welcome to SS Lab 02 App</h1>' | sudo tee /var/www/html/index.html
-sudo mkdir /var/www/html/app2
-sudo echo '<!DOCTYPE html> <html> <body style="background-color: rgb(250, 210, 210);"> <h1>Welcome to SS Lab 02 App</h1></h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body> </html>' | sudo tee /var/www/html/app2/index.html
