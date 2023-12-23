7a15f9ca3f61189e7a3dbbc262bdd04f1a9c1d19
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-23 23:58:26 +0530

    changes reverted in bash file

--- lab-ss-03-terraform-settings-providers-and-resources/install-apache-server.sh
+++ lab-ss-03-terraform-settings-providers-and-resources/install-apache-server.sh
@@ -3,6 +3,6 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start
-sudo echo '<h1>Welcome to Terraform Variables & Datasources</h1>' | sudo tee /var/www/html/index.html
+sudo echo '<h1>Welcome to Terraform Demo</h1>' | sudo tee /var/www/html/index.html
 sudo mkdir /var/www/html/app
-sudo echo '<!DOCTYPE html> <html> <body style="background-color: rgb(211, 211, 211);"> <h1>Welcome to Terraform Variables & Datasources</h1> <p>application version: 1.0.0</p> </body> </html>' | sudo tee /var/www/html/app/index.html
+sudo echo '<!DOCTYPE html> <html> <body style="background-color: rgb(211, 211, 211);"> <h1>Welcome to Terraform Demo</h1> <p>application version: 1.0.0</p> </body> </html>' | sudo tee /var/www/html/app/index.html
