52a9e0cb961d2ea89aaed127af30bb80d50af3a3
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-24 08:48:14 +0530

    terraform for loops, lists, maps & count meta-argument

---
+++ lab-ss-05-01-terraform-loops-metaarguments-splatoperator/main.tf
@@ -0,0 +1,19 @@
+####################################################################################################
+# Date: 12/23/2023
+# Author: Sagar Pitalekar
+# Title: Terraform Loops MetaArguments SplatOperator - MetaArgument Count For Loops Lists Maps
+# Description: In this lab, we demonstrate terraform for loops, lists, maps & count meta-argument.
+####################################################################################################
+
+# configure aws ec2 instance
+resource "aws_instance" "labss0501" {
+  ami = data.aws_ami.amzlinux2023.id
+  instance_type = var.instance_type
+  user_data = file("${path.module}/install-apache-server.sh")
+  key_name = var.instance_keypair
+  vpc_security_group_ids = [aws_security_group.vpc-labss05-ssh.id, aws_security_group.vpc-labss05-web.id]
+  count = 2
+  tags = {
+    Name = "count-demo-${count.index}"
+  }
+}
