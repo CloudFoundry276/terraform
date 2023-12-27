415b5ed05508b5438ec8c704d53fe1b8c425920e
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-27 23:56:15 +0530

    cidr correction

--- lab-iam-02-vpc-with-ec2/main.tf
+++ lab-iam-02-vpc-with-ec2/main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "labiam02-sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0./0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "allow port 80"
