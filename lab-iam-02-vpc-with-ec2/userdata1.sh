2cc5365646e5576f718312c6aeab1beabf485373
CloudFoundry276 <cloudfoundry276@gmail.com>
2023-12-27 23:53:36 +0530

    terraform vpc with ec2 instance

---
+++ lab-iam-02-vpc-with-ec2/userdata1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y httpd
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+sudo systemctl enable httpd
+sudo service httpd start
+sudo echo '<h1>Welcome to Terraform Demo</h1>' | sudo tee /var/www/html/index.html
+sudo mkdir /var/www/html/app
+sudo echo '<!DOCTYPE html> <html> <head> <title>Welcome Page 2</title> <style> @keyframes colorChange { 0% { color: red; } 50% { color: green; } 100% { color: blue; } } h1 { animation: colorChange 2s infinite; } </style> </head> <body> <h1>Hello, World from Terraform 2!</h1> <h2>Instance ID: <span style="color:green">'$INSTANCE_ID'</span></h2> <p>Welcome to Terraform Demo 2</p> </body>
+</html>' | sudo tee /var/www/html/app/index.html
---
+++ lab-iam-02-vpc-with-ec2/variables.tf
@@ -0,0 +1,4 @@
+variable "cidr" {
+  description = "default cidr for vpc"
+  default = "10.0.0.0/16"
+}
