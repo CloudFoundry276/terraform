154619f2520409d7e42b65ee79e9c3503921f82c
CloudFoundry276 <cloudfoundry276@gmail.com>
2024-01-01 22:49:53 +0530

    tags name added for ec2 instance

--- lab-iam-03-terraform-modules/modules/ec2-instance/main.tf
+++ lab-iam-03-terraform-modules/modules/ec2-instance/main.tf
@@ -8,4 +8,7 @@ resource "aws_instance" "labiam03-ec2" {
   ami = var.ami_value
   instance_type = var.instance_type_value
   subnet_id = var.subnet_id_value
+  tags = {
+    Name = "labiam03-ec2"
+  }
 }
