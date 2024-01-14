e2a299e10e771320e1d46e752d04b30066db6557
CloudFoundry276 <cloudfoundry276@gmail.com>
2024-01-14 14:15:51 +0530

    bash file updated

--- terraform-aws-labs/lab-03-terraform-settings-providers-resources/app-install.sh
+++ terraform-aws-labs/lab-03-terraform-settings-providers-resources/app-install.sh
@@ -3,8 +3,8 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start
-sudo echo '<h1>Hello World from $(hostname -f)</h1>' | sudo tee /var/www/html/index.html
+sudo echo '<h1>Welcome to Terraform App</h1>' | sudo tee /var/www/html/index.html
 sudo mkdir /var/www/html/app
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(211, 211, 211);"> <h1>Hello World from $(hostname -f)</h1> </body></html>' | sudo tee /var/www/html/app/index.html
+sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(211, 211, 211);"> <h1>Welcome to Terraform App</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app/index.html
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 sudo curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app/metadata.html
